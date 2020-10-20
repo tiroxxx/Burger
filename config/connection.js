2b541ee01d45eae5bc66de43578372bb3401047c
tiroxxx <christianmarquez41@gmail.com>
2020-10-19 21:22:35 -0400

    finished adding all connection info for jawsdb

--- config/connection.js
+++ config/connection.js
@@ -6,11 +6,11 @@ if (process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: "localhost",
+    host: "vrk7xcrab1wsx4r1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port: 3306,
-    user: "root",
-    password: "root",
-    database: "burgers_db"
+    user: "	vnpdkgmheo93xhci",
+    password: "v0rixgzsny5is14q",
+    database: "zyem1qzcribaezyc"
   });
 };
  //look for the error
